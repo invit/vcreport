fa973c52abbea9a04c284f651fb4236e452b69fb
Matthias Blaser <git@mooch.ch>
2021-05-29 15:38:05 +0200

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,165 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/invit/vcreport/internal/lib/version"
+	"github.com/olekukonko/tablewriter"
+	"github.com/prometheus/common/expfmt"
+	"github.com/spf13/cobra"
+)
+
+type Pod struct {
+	Namespace string `json:"namespace"`
+	Pod       string `json:"pod"`
+	Container string `json:"container"`
+}
+
+type Image struct {
+	Image          string `json:"image"`
+	CurrentVersion string `json:"current_version"`
+	LatestVersion  string `json:"latest_version"`
+	Pods           []Pod  `json:"pods"`
+}
+
+func (i *Image) AddPod(p Pod) {
+	i.Pods = append(i.Pods, p)
+}
+
+func init() {
+	rootCmd.Flags().BoolP("all", "a", false, "Show all images, not just outdated ones")
+	rootCmd.Flags().BoolP("brief", "b", false, "Just show images, but no pods")
+}
+
+var rootCmd = &cobra.Command{
+	Use:           "vcreport metrics-url",
+	Short:         "Displays a human-readable report from version-checker metrics",
+	SilenceErrors: true,
+	Version:       fmt.Sprintf("%s-%s", version.Version, version.Commit),
+	Args:          cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		displayAll, _ := cmd.Flags().GetBool("all")
+		displayBrief, _ := cmd.Flags().GetBool("brief")
+
+		c := http.Client{}
+		resp, err := c.Get(args[0])
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusOK {
+			p := expfmt.TextParser{}
+			mf, err := p.TextToMetricFamilies(resp.Body)
+
+			if err != nil {
+				return err
+			}
+
+			images := map[string]*Image{}
+
+			for _, v := range mf {
+				for _, m := range v.Metric {
+					labels := map[string]string{}
+
+					for _, l := range m.Label {
+						labels[l.GetName()] = l.GetValue()
+					}
+
+					if !displayAll && labels["current_version"] == labels["latest_version"] {
+						continue
+					}
+
+					key := fmt.Sprintf("%s:%s", labels["image"], labels["current_version"])
+
+					if _, ok := images[key]; !ok {
+						images[key] = &Image{
+							Image:          labels["image"],
+							CurrentVersion: labels["current_version"],
+							LatestVersion:  labels["latest_version"],
+							Pods: []Pod{{
+								Namespace: labels["namespace"],
+								Pod:       labels["pod"],
+								Container: labels["container"],
+							}},
+						}
+					} else {
+						images[key].AddPod(
+							Pod{
+								Namespace: labels["namespace"],
+								Pod:       labels["pod"],
+								Container: labels["container"],
+							},
+						)
+					}
+				}
+			}
+
+			var keys = []string{}
+
+			for k, _ := range images {
+				keys = append(keys, k)
+			}
+
+			sort.Strings(keys)
+
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetAlignment(tablewriter.ALIGN_LEFT)
+			table.SetRowLine(true)
+			//table.SetRowSeparator("-")
+
+			if displayBrief {
+				table.SetHeader([]string{"Image", "Current", "Latest"})
+
+				for _, k := range keys {
+					table.Append([]string{
+						images[k].Image,
+						images[k].CurrentVersion,
+						images[k].LatestVersion,
+					})
+				}
+
+			} else {
+				table.SetHeader([]string{"Image", "Current", "Latest", "Pods"})
+
+				for _, k := range keys {
+					pods := []string{}
+
+					for _, p := range images[k].Pods {
+						pods = append(
+							pods,
+							fmt.Sprintf("%s/%s/%s", p.Namespace, p.Pod, p.Container),
+						)
+					}
+
+					table.Append([]string{
+						images[k].Image,
+						images[k].CurrentVersion,
+						images[k].LatestVersion,
+						strings.Join(pods, "\n"),
+					})
+				}
+			}
+
+			table.Render()
+		} else {
+			b, _ := io.ReadAll(resp.Body)
+			return fmt.Errorf("Error from %s (Code %d): %s", args[0], resp.StatusCode, b)
+		}
+
+		return nil
+	},
+}
+
+// Execute runs root command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/invit/vcreport
+
+go 1.16
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/prometheus/common v0.25.0 // indirect
+	github.com/spf13/cobra v1.1.3
+)
